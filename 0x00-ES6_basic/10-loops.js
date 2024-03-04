c69f05d62c360068e1698026927997e042994e9c
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-03-04 18:25:19 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const Newvalue = [];
-  for (let value of array) {
-   Newvalue.push(appendString + value);
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return Newvalue;
+  return newArray;
 }
