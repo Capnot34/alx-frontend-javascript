57d322ffcb121887f24377162615453ff46da93e
Capnot34 <jenniferamachree2@gmail.com>
2024-03-04 17:38:36 +0000

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,10 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
-  [`$(departmentName}`]: employees,
-  };
+  // Create an object with the specified format
+  const employeesObject = {};
+
+  // Use the provided departmentName as a dynamic property name
+  employeesObject[departmentName] = employees;
+
+  // Return the created object
+  return employeesObject;
 }
