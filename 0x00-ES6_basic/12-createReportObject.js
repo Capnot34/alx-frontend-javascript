1b737d50893981ab5b7a4b3be35710c06e2a2673
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-03-04 18:49:25 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,13 +1,8 @@
 export default function createReportObject(employeesList) {
-  // Create an object to store department names and employees
-  const allEmployees = {...employeesList};
-
-  // Define a method property to get the number of departments
-  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
-
-  // Return an object with allEmployees and getNumberOfDepartments method
   return {
-    allEmployees,
-    getNumberOfDepartments
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
