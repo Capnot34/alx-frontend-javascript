0e9cc50e4c37d93dd746eb9c87e6fef7d0d6e575
Capnot34 <jenniferamachree2@gmail.com>
2024-03-06 21:54:32 +0000

    Task 0 done

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call, for example:
+    setTimeout(() => {
+      // Resolve the promise with some data
+      resolve("Response from API");
+      
+      // Or reject the promise if there's an error
+      // reject(new Error("Failed to get response from API"));
+    }, 2000); // Simulating a delay of 2 seconds
+  });
+}
