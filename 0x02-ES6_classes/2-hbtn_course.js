f2cb01f0c3c912521b49774eb42c588d31c0c89f
Capnot34 <jenniferamachree2@gmail.com>
2024-03-07 22:56:26 +0000

    task 2 done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+
+    // Validate attribute types
+    if (typeof this._name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof this._length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
