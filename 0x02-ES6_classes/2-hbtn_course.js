5f2080c729b16e5ebaee8533b276f57e3c87f078
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-03-08 00:11:51 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,23 +1,13 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-
-    // Validate attribute types
-    if (typeof this._name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof this._length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter and setter for name
-  get name() {
-    return this._name;
-  }
-
+  /**
+   * @param {String} name
+   */
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -25,11 +15,13 @@ export default class HolbertonCourse {
     this._name = name;
   }
 
-  // Getter and setter for length
-  get length() {
-    return this._length;
+  get name() {
+    return this._name;
   }
 
+  /**
+   * @param {Number} length
+   */
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
@@ -37,12 +29,24 @@ export default class HolbertonCourse {
     this._length = length;
   }
 
-  // Getter and setter for students
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * @param {Array} students
+   */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
+    }
+  }
+
   get students() {
     return this._students;
   }
-
-  set students(students) {
-    this._students = students;
-  }
 }
+
+export default HolbertonCourse;
