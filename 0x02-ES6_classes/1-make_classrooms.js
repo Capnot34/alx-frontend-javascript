df8f0b9202359315d9052af3bfc624683e292690
Capnot34 <jenniferamachree2@gmail.com>
2024-03-07 14:13:47 +0000

    attempted task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const Rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+    ];
+    return Rooms;
+}
+export default initializeRooms;
