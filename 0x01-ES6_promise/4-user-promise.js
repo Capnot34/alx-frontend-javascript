c5e04fa6a93742dcecf2895f5e5104c4e405e939
Capnot34 <jenniferamachree2@gmail.com>
2024-03-08 19:40:52 +0000

    task 4 done

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 function signUpUser(firstName, lastName) {
-  return promise.resolve({
-    firstName: `${firstName}`,
-    lastName: `${lastName}`,
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName
   });
 }
 
