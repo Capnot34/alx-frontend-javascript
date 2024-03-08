dcc254d01de4d32b1fa9426c9e061222d5bf7b3d
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-03-08 21:20:08 +0100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,16 +4,15 @@ async function asyncUploadUser() {
   try {
     const photoResponse = await uploadPhoto();
     const userResponse = await createUser();
-    
     return {
       photo: photoResponse,
-      user: userResponse
+      user: userResponse,
     };
   } catch (error) {
     console.error('An error occurred:', error);
     return {
       photo: null,
-      user: null
+      user: null,
     };
   }
 }
