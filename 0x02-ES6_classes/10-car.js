6d0df70aa9459d29f939f98922aec855f94689ba
Capnot34 <jenniferamachree2@gmail.com>
2024-03-08 14:15:49 +0000

    done

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,12 +1,16 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
 class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(this[_brand], this[_motor], this[_color]);
   }
 }
 
