e00e1bf5dd01fd76312f0651cd50a3f375cdb9bb
Capnot34 <jenniferamachree2@gmail.com>
2024-03-08 12:37:45 +0000

    attempted task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,22 @@
 class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
 
