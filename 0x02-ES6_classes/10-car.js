0af266e94d381f5c991c886ba23f750b3a1567bc
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-03-08 15:18:50 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,17 +1,17 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    const Species = this.constructor[Symbol.species];
+
+    return Object.create(this.constructor.prototype);
   }
 }
-
-export default Car;
